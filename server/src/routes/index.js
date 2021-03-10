ea29ee4facc73e3ff68e96aa03023c7ce1f730ed
Edofo <48387251+Edofo@users.noreply.github.com>
2021-03-10 13:36:03 +0100

    Update index and task

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import auth from './auth'
+import task from './task'
 import secured from './secured'
 import passport from 'passport'
 
@@ -9,16 +10,8 @@ api.get('/', (_req, res) => {
   res.json({ hello: "world" })
 })
 
-api.get('/students', (_req, res) => {
-  res.json([
-    { firstname: 'Aymene' },
-    { firstname: 'Bakary' },
-    { firstname: 'Jean-Loris' }
-  ])
-})
-
-
 api.use('/auth', auth)
+api.use('/task', task)
 api.use('/', passport.authenticate('jwt', { session: false }), secured)
 
 export default api
--- server/src/routes/task.js
+++ server/src/routes/task.js
@@ -6,6 +6,8 @@ api.get('/task/:id', (_req, res) => {
     const id = parseInt(req.params.id, 10)
 
     const userverif =  pokedex.find(c => userId === id);
+
+    res.json({ data: { task } })
 })
 
 export default api
